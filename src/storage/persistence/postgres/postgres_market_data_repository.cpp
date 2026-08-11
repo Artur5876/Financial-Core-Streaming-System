00ce5e000f4221ed587f33b8584bd42bfdd1819a
artur <artur@debian13.agg2.mdn.lmk-mlw.eir.ie>
2026-08-11 14:28:06 +0100

    Added a few constants in postgres_market_repository.cpp but didnt completed the methods

--- src/storage/persistence/postgres/postgres_market_data_repository.cpp
+++ src/storage/persistence/postgres/postgres_market_data_repository.cpp
@@ -6,7 +6,19 @@
 
 namespace fincore::persistence::postgres {
     namespace {
-        using domain
+        using domain::Decimal;
+        using domain::Timestamp;
+
+        [[nodiscard]] std::string timestamp_to_utc_text(Timestamp timestamp) {
+            using namespace std::chrono;
+
+            const auto micros = floor<microseconds>(timestamp);
+            const auto seconds_part = floor<seconds>(micros);
+            const auto fractional = duration_cast<microseconds>(micros - seconds_part).count();
+            const std::time_t time = system_clock::
+
+        }
+
     }
 }
 
